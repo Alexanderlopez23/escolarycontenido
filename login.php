08f528fe36dd77b5792ac0f566fac277d9f600ba
Alex <jalopez9722@misena.edu.co>
2020-09-19 16:16:43 -0500

    Se mejora la vista de login dando una interfaz con colores suaves, se descarga libreria sweet alert

--- login.php
+++ login.php
@@ -12,39 +12,52 @@ if (isset($_SESSION['mensaje'])) {
     <head>
         <meta charset="UTF-8">	
         <title> INICIAR SESION  </title>
-        <link rel="stylesheet" type="text/css" href="css/css.css">
+        <!-- LIBRERIA SWEET -->
+        <script src="librerias/sweetalert/package/dist/sweetalert2.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="librerias/sweetalert/package/dist/sweetalert2.min.css"> 
         <script type="text/javascript" src="javascript/funciones.js" defer></script>
         <script type="text/javascript" src="javascript/md5.js"></script>   
         <link rel="shortcut icon" href="imagenes/Favicon.ico">
+        <link rel="stylesheet" href="css/formularioregistrocss.css">
     </head>
     <body>
+        <form role="form" method="POST" action="Controlador.php" name="formLogin">
+            <section class="form-register">
+                <h4> <center>Iniciar Sesión </center></h4>
 
-        <div class="login-box">
-            <h1> INICIAR SESIÓN </h1>
-            <form role="form" method="POST" action="Controlador.php" name="formLogin">
-                <!--User name -->
-                <Label for="username"> USUARIO </Label>	
-                <input type="text" value="" id="InputCorreo" name="email" placeholder="CORREO" autofocus >
-                <!-- Password -->
+                <Label for="username"> CORREO ELECTRONICO</Label>
+                <br>
+                <br>
+                <input  class="controls" type="email" value="" id="InputCorreo" name="email" placeholder="CORREO" autofocus >
+                <br>
+
                 <Label for="Password"> CONTRASEÑA </Label>
-
-                <input type="Password" value="" id="InputPassword" name="password" placeholder="CONTRASEÑA ">
-
-                <input type="button" value="Ingresar" onclick="validar_logueo()" name="" value="Ingresar" >
                 <br>
                 <br>
+
+                <input class="controls" type="Password" value="" id="InputPassword" name="password" placeholder="CONTRASEÑA ">
+                <br>
+
+                <input class="botons"type="button" value="Ingresar" onclick="validar_logueo()" name="" value="Ingresar" >
+                <br>
+
                 <input type="hidden" name="ruta" value="gestionDeAcceso">
                 <br>
                 <label>¿No tienes usuario?</label>
                 <a  onclick="invitado();" href="registro.php"> REGISTRATE </a>
-            </form>
-        </div>
+            </section>
+        </form>
 
-        <script type="text/javascript">
-            function invitado()
-            {
-                alert("Se registrara como un usuario invitado mientras el desarrollador define sus funciones en el sistema de informacion");
-            }
-        </script>
-    </body>
+        <script languaje="javascript">
+           function invitado()  {
+               
+            $mensaje="Se registrara como un usuario invitado mientras el desarrollador define sus funciones en el software";
+               
+            alert ( $mensaje );
+           }
+        </Script>
+        <!-- LIBRERIA SWEET -->
+    <script src="librerias/sweetalert/package/dist/sweetalert2.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="librerias/sweetalert/package/dist/sweetalert2.min.css">    
+</body>   
 </html>
