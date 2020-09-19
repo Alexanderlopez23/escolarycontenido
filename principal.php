a08d7f1800c2756ca86fe96e89b4b45c2d69dfc5
Alex <jalopez9722@misena.edu.co>
2020-09-19 10:07:21 -0500

    Se vincula el home page cada vez que se da click en INTRANET EYC

--- principal.php
+++ principal.php
@@ -34,7 +34,7 @@ if (isset($_SESSION['mensaje'])) {
      </head>
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            <a class="navbar-brand">INTRANET EYC</a>
+            <a class="navbar-brand" href="Controlador.php?ruta=mostrarinicio">INTRANET EYC</a>
             <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
             <!-- Navbar Search-->
             <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
